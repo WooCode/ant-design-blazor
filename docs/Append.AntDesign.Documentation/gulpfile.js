3eece8e621d74076fc3adbe0f26176faf8414a94
Joris Claeys <joris.claeys12@gmail.com>
2020-06-03 11:00:43 +0200

    [Gulp] compile less configfiles

---
+++ docs/Append.AntDesign.Documentation/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp'),
+    cleanCss = require('gulp-clean-css'),
+    less = require('gulp-less');
+var rename = require('gulp-rename');
+
+gulp.task('less', function () {
+    return gulp
+        .src('style/index.less')
+        .pipe(less({ javascriptEnabled: true }))
+        .pipe(cleanCss({ compatibility: 'ie8' }))
+        .pipe(rename('documentation-styles.css'))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+//add more tasks to default with parallel example: gulp.parallel('less', 'scripts', 'javascript')
+gulp.task('default', gulp.parallel('less'));
---
+++ source/Append.AntDesign/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp'),
+    cleanCss = require('gulp-clean-css'),
+    less = require('gulp-less');
+var rename = require('gulp-rename');
+
+gulp.task('less', function () {
+    return gulp
+        .src('wwwroot/css/ant-design-blazor.less')
+        .pipe(less({ javascriptEnabled: true }))
+        .pipe(cleanCss({ compatibility: 'ie8' }))
+        .pipe(rename('ant-design-blazor.min.css'))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+//add more tasks to default with parallel example: gulp.parallel('less', 'scripts', 'javascript')
+gulp.task('default', gulp.parallel('less'));
