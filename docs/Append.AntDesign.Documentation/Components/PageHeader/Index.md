9ab82f44ba0c780b1faa654fc061d2ef8df45732
Joris Claeys <joris.claeys12@gmail.com>
2020-06-05 16:06:30 +0200

    [PageHeader] implementatie

---
+++ docs/Append.AntDesign.Documentation/Components/PageHeader/Index.md
@@ -0,0 +1,20 @@
+﻿A header with common actions and design elements built in.
+
+## When To Use
+
+PageHeader can be used to highlight the page topic, display important information about the page, and carry the action items related to the current page (including page-level operations, inter-page navigation, etc.) It can also be used as inter-page navigation.
+
+## API
+
+| Param | Description | Type | Default value | Version |
+| --- | --- | --- | --- | --- |
+| Title | Custom title text | RenderFragment | - |  |
+| SubTitle | Custom subtitle text | RenderFragment | - |  |
+| Ghost | PageHeader type, will change background color | bool | true |  |
+| Avatar | Avatar next to the title bar | [Avatar](/components/avatar/) | - |  |
+| BackIcon | Custom back icon, if false the back icon will not be displayed | RenderFragment \| bool | `<ArrowLeft />` |  |
+| Tags | Tag list next to title | [Tag](/components/tag/)[] \| [Tag](/components/tag/) | - |  |
+| Extra | Operating area, at the end of the line of the title line | RenderFragment | - |  |
+| Breadcrumb | Breadcrumb configuration | [Breadcrumb](/components/breadcrumb/) | - |  |
+| Footer | PageHeader's footer, generally used to render TabBar | RenderFragment | - |  |
+| OnBack | Back icon click event | `()=>void` | `()=>history.back()` |  |
---
+++ source/Append.AntDesign/Components/PageHeader/PageHeader.razor.cs
@@ -0,0 +1,60 @@
+﻿using Append.AntDesign.Core;
+using Append.AntDesign.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Append.AntDesign.Components
+{
+    public partial class PageHeader
+    {
+
+        private static string prefix = "ant-page-header";
+
+        private ClassBuilder classes => ClassBuilder.Create(Class)
+                .AddClass(prefix)
+                .AddClassWhen("has-breadcrum", Breadcrumb != null)
+                .AddClassWhen("has-footer", Footer != null)
+                .AddClassWhen($"{prefix}-ghost", Ghost)
+                .AddClassWhen($"{prefix}-compact", compact);
+
+        [Inject] public IWindowService WindowService { get; set; }
+        [Parameter] public RenderFragment<Avatar> Avatar { get; set; }
+        [Parameter] public RenderFragment BackIcon { get; set; }
+        [Parameter] public Breadcrumb Breadcrumb { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public RenderFragment Extra { get; set; }
+        [Parameter] public RenderFragment Footer { get; set; }
+        [Parameter] public bool Ghost { get; set; } = true;
+        //give OnBack the previous visitted website as default
+        [Parameter] public EventCallback OnBack { get; set; }
+        [Parameter] public RenderFragment SubTitle { get; set; }
+        [Parameter] public RenderFragment<Tag> Tags {get; set; }
+        [Parameter] public RenderFragment Title { get; set; }
+
+        private bool compact { get; set; }
+        private void UpdateCompact(int windowWidth)
+        {
+            compact = windowWidth < BreakpointType.Md.Width;
+            StateHasChanged();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                var dimensions = await WindowService.GetDimensions();
+                await WindowService.RegisterOnWindowResizeHandler(this);
+                UpdateCompact(dimensions.Width);
+            }
+        }
+
+        [JSInvokable]
+        public void OnWindowResize(int windowWidth)
+        {
+            UpdateCompact(windowWidth);
+        }
+    }
+}
