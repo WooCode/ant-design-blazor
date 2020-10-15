2d73d4bbdd8105175bea91fa64038ab1b49e30ee
Petter Ahlqvist <petter@woocode.com>
2020-10-15 10:43:56 +0200

    Moving from mac to windows

---
+++ docs/Append.AntDesign.Documentation/Components/Card/Index.md
@@ -0,0 +1,26 @@
+﻿Card test
+## When To Use
+
+
+
+## API
+
+
+
+## FAQ
+> Ask your questions on Github, they might end up here.
+
+
+<style>
+[id^=components-button-demo-] .ant-btn {
+  margin-right: 8px;
+  margin-bottom: 12px;
+}
+[id^=components-button-demo-] .ant-btn-group > .ant-btn,
+[id^=components-button-demo-] .ant-btn-group > span > .ant-btn {
+  margin-right: 0;
+}
+[data-theme="dark"] .site-button-ghost-wrapper {
+  background: rgba(255, 255, 255, 0.2);
+}
+</style>
---
+++ source/Append.AntDesign/Components/Card/Card.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Append.AntDesign.Core;
+using Microsoft.AspNetCore.Components;
+
+namespace Append.AntDesign.Components
+{
+    public partial class Card
+    {
+        private static readonly string prefix = "ant-card";
+
+        private ClassBuilder Classes => ClassBuilder.Create(Class)
+            .AddClass(prefix)
+            .AddClassWhen($"{prefix}-bordered", Bordered);
+
+        [Parameter] public bool Bordered { get; set; }
+        [Parameter] public bool Loading { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [Parameter] public string HeadStyle { get; set; }
+        [Parameter] public string BodyStyle { get; set; }
+    }
+}
